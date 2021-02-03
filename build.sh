0ae717f5c7a49acc1cc1fcfa2ed6b26fec2a6a19
Matthias Zöchling <github@cssence.com>
2021-02-03 19:36:13 +0100

    404.html noindex nofollow

--- build.sh
+++ build.sh
@@ -5,4 +5,5 @@ titleline=5
 head -n $((titleline - 1)) $input > $output
 echo '<title>404 — Page not found</title>' >> $output
 echo '<meta name="description" content="On https://cv.cssence.com/, this page does not exist.">' >> $output
+echo '<meta name="robots" content="noindex,follow">' >> $output
 tail -n +$((titleline + 2)) $input >> $output
